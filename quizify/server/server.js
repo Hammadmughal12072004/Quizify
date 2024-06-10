92e8bbb90eadcfbbfa0148a054545db1d44e9889
hammadmughal12072004 <hammadanwaar04@gmail.com>
2024-06-10 12:06:47 +0500

    changes in server.js

--- quizify/server/server.js
+++ quizify/server/server.js
@@ -42,6 +42,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
+
+
+  if (process.env.NODE_ENV == 'production'){
+    app.use(express.static('server/client/build'));
+    const path = require("path");
+    app.get("/",(req, res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+  }
+
+
+
 const PORT = process.env.PORT || 5000; // Use environment port or 5000 as default
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
